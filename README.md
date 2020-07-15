00471f05d4ac859fd74a222e999c950d4a797662
Ivo Branco <ivo.branco@fccn.pt>
2020-07-15 23:40:39 +0100

    Add arquivo 404 js file.

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-# Usage example of CDX Server API
+# Web archive not found 404 fallback
 
-## soft-404.hml
-Only show a link to Arquivo.pt if there is at least a version of site.
+Web archive not found HTTP status 404 ECMAScript handler that uses Memento API (rfc 7089).
 
-# Test it
-To test the example go to: https://htmlpreview.github.io/?https://github.com/arquivo/example-cdx-api/blob/master/soft-404.html
+By default uses the Arquivo.pt Memento API, but any other could be used, if its CORS allow it configuration.
+
+## CORS
+Requires that the Memento API have an allowed CORS policy.
+In practive the web archive server should return the response HTTP header: \"Access-Control-Allow-Origin: \*\"
+
+## Simples use
+
+On the place where you want to have the message.
+```js
+<script type="text/javascript" src="arquivo404.js" async defer onload="ARQUIVO_NOT_FOUND_404.call();"></script>
+```
+
+## Customizations
+
+### Message
+
+```js
+<script type="text/javascript">
+	function start404() {
+		ARQUIVO_NOT_FOUND_404
+			.messages([['pt', '<a href="{archivedURL}">Visite uma versão anterior desta página de {day} {monthLong}, {year}.</a>']])
+			.call();
+	}
+</script>
+<script type="text/javascript" src="arquivo404.js" async defer onload="start404();"></script>
+```
+
+### Languages
---
+++ arquivo404.js
@@ -0,0 +1,190 @@
+/**
+ * Web archive not found HTTP status 404 ECMAScript handler that uses Memento API (rfc 7089).
+ * Requires that the Memento API have an allowed CORS policy.
+ * In practive the server should return the response HTTP header: "Access-Control-Allow-Origin: *"
+ *
+ */
+var ARQUIVO_NOT_FOUND_404 = ARQUIVO_NOT_FOUND_404 || (function(){
+
+	var _url = window.location.href;
+	var _handled = false;
+	var _messageContainerId = null;
+	var _language = navigator.language || navigator.userLanguage;
+
+	var _messagesMap = new Map([ 
+		['en', 'You can view an archived versions of this page at {year} <a href="{archivedURL}">{archiveName}</a>'],
+		['pt', 'Poder&aacute; ver uma vers&atilde;o arquivada no <a href="{archivedURL}">{archiveName}</a>'] 
+	]);
+
+	// List of archives to try
+	// timeout in miliseconds
+	var _archives = [
+		{ timeout: 2000, archiveName: "Arquivo.pt", archiveApiUrl: "https://arquivo.pt/wayback/timemap/link/" } , 
+		//{ timeout: 5000, archiveName: "Internet Archive", archiveApiUrl: "https://cors-anywhere.herokuapp.com/http://web.archive.org/web/timemap/link/" }
+	];
+
+	function handleMessage(message) {
+		if (!!_messageContainerId) {
+			document.getElementById(_messageContainerId).innerHTML = message;
+		} else {
+			document.write(message);
+		}
+	}
+
+	function formatMessage(archive, archivedURL, date) {
+		var message = _messagesMap.get(_language);
+		if (!message) {
+			const shortUserLang = _language.substring(0, _language.indexOf('-'));
+			message = _messagesMap.get(shortUserLang);
+		} 
+		if (!message) {
+			// returns first message
+			message = _messagesMap.values().next().value;
+		}
+
+		const replaceMap = new Map( [ 
+			['archiveName', archive.hasOwnProperty('archiveName') ?  archive.archiveName : ''],
+			['archivedURL', archivedURL],
+			['year', date.getFullYear()],
+			['month', date.getMonth()],
+			['monthLong', new Intl.DateTimeFormat(_language, {  month: 'long' }).format(date)],
+			['day', date.getDate()],
+			['hour', date.getHours()],
+			['minute', date.getMinutes()],
+			['second', date.getSeconds()],
+			['millisecond', date.getMilliseconds()],
+		]);
+		replaceMap.forEach(function(value, key){
+			message = message.replace(new RegExp('{' + key + '}', 'gi'), value);
+		});
+
+		return message;
+	}
+
+	function getArchivedURL(mementoLine) {
+		const idxFirstSpace = mementoLine.indexOf(' ');
+		return mementoLine.substring(1, idxFirstSpace-3);
+	}
+
+	function getMementoDate(mementoLine) {
+		const datetimeSnip = ' datetime="';
+		const idxDatetime = mementoLine.indexOf(datetimeSnip);
+		const dateRFC1123InitialPosition = idxDatetime + datetimeSnip.length;
+		const datePartial = mementoLine.substring(dateRFC1123InitialPosition);
+		const dateRFC1123 = datePartial.substring(0, datePartial.indexOf('"'));
+		const date = new Date(dateRFC1123);
+		return date;
+	}
+
+	function handleMemento(archive, pageUrl, mementoResponse) {
+		if (!_handled) {
+			var mementoLines = mementoResponse.split(/\r?\n/);
+			const linesCount = mementoLines.length;
+			if (linesCount >= 2) {
+				const lastMemento = mementoLines[linesCount-2];
+
+				const archivedURL = getArchivedURL(lastMemento)
+				const date = getMementoDate(lastMemento)
+
+				if (!_handled) {
+					_handled = true;
+					handleMessage(formatMessage(archive, archivedURL, date));
+				}
+			}
+		}
+	}
+
+	function getMementoUsingXMLHttpRequest( archive ) {
+		var pageUrl = _url;
+		var xmlurl = archive.archiveApiUrl + pageUrl;
+
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+	        if (xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
+	        	if (xmlhttp.status == 200) {
+	        		handleMemento(archive, pageUrl, xmlhttp.responseText);
+	        	}
+	        	else if (xmlhttp.status == 400) {
+	        		console.log('There was an error 400');
+	        	}
+	        	else {
+	        		console.log('something else other than 200 was returned');
+	        	}
+	        }
+	    };
+	    xmlhttp.open("GET", xmlurl, true);
+	    xmlhttp.timeout = archive.hasOwnProperty('timeout') ? archive.timeout : 5000;
+	    xmlhttp.send();
+	}
+
+	return {
+
+		// Replace all the messages on a single method.
+		// Example:
+		//   .messages([
+		//         ['pt', '<a href="{archivedURL}">Visite uma versão anterior desta página de {day} {monthLong}, {year} no {archiveName}.</a>'],
+		//         ['en', '<a href="{archivedURL}">Visit an earlier version of this page from {day} {monthLong}, {year} at {archiveName}.</a>'],
+		//      ])
+		messages : function(langMessagesMap) {
+			_messagesMap = new Map(langMessagesMap);
+			return this;
+		},
+
+		// Change default language.
+		language : function(language) {
+			_language = language;
+			return this;
+		},
+
+		// Only use a single message for all language.
+		// Example:
+		//   .message('You can view an archived versions on <a href="{archivedURL}">{archivedURL}</a>');
+		message : function(message) {
+			_messagesMap = new Map([ ['', message] ]);
+			return this;
+		},
+
+		// Add a language message
+		// Example:
+		//   .addMessage('pt', 'You can view an archived versions on <a href="{archivedURL}">{archivedURL}</a>');
+		addMessage : function(language, message) {
+			_messagesMap.set(language, message);
+		},
+
+		// Put the message on a specific element
+		messageContainerId : function(messageContainerId) {
+			_messageContainerId = messageContainerId;
+			return this;
+		},
+
+		// Append a new archive to search.
+		// Example:
+		//   .addArchive( {timeout: 2000, archiveName: "Arquivo.pt Preprod", archiveApiUrl: "https://preprod.arquivo.pt/wayback/timemap/link/"} )
+		addArchive : function(archive) {
+			// timeout: 2000, archiveName: "Arquivo.pt", archiveApiUrl
+			const o = archive.hasOwnProperty('archiveApiUrl') ? archive : {'archiveApiUrl' : archive} ;
+			_archives.push(o)
+			return this;
+		},
+
+		archive : function(archive) {
+			_archives = [];
+			this.addArchive(archive);
+			return this;
+		},
+
+		// Change url to searhc on web archives instead of the current one.
+		url : function(url) {
+			_url = url;
+			return this;
+		},
+
+		// method to be called to start the web archives search.
+		call : function (url) {
+			for ( let i = 0 ; i < _archives.length ; i ++) {
+				const archive = _archives[i];
+				getMementoUsingXMLHttpRequest(archive);
+			}
+		}
+    };
+}());
